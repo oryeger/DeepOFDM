5b6e924d2e4d7f2033f690ba812d2f0818be481e
Ory Eger <egerory@gmail.com>
2025-04-27 15:39:25 +0300

    add sionna tdl initial file

---
+++ python_code/channel/sionna/TLD_channel_example.py
@@ -0,0 +1,178 @@
+import os
+if os.getenv("CUDA_VISIBLE_DEVICES") is None:
+    gpu_num = 0 # Use "" to use the CPU
+    os.environ["CUDA_VISIBLE_DEVICES"] = f"{gpu_num}"
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+# Import Sionna
+try:
+    import sionna
+except ImportError as e:
+    # Install Sionna if package is not already installed
+    import os
+    os.system("pip install sionna")
+    import sionna
+
+import tensorflow as tf
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    try:
+        tf.config.experimental.set_memory_growth(gpus[0], True)
+    except RuntimeError as e:
+        print(e)
+tf.get_logger().setLevel('ERROR')
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    try:
+        tf.config.experimental.set_memory_growth(gpus[0], True)
+    except RuntimeError as e:
+        print(e)
+tf.get_logger().setLevel('ERROR')
+
+# Set random seed for reproducibility
+sionna.config.seed = 42
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+import time
+
+from sionna.mimo import StreamManagement
+
+from sionna.ofdm import ResourceGrid, ResourceGridMapper, LSChannelEstimator, LMMSEEqualizer
+from sionna.ofdm import OFDMModulator, OFDMDemodulator, ZFPrecoder, RemoveNulledSubcarriers
+
+from sionna.channel.tr38901 import AntennaArray, CDL, TDL, Antenna
+from sionna.channel import subcarrier_frequencies, cir_to_ofdm_channel, cir_to_time_channel, time_lag_discrete_time_channel
+from sionna.channel import ApplyOFDMChannel, ApplyTimeChannel, OFDMChannel, TimeChannel
+
+from sionna.fec.ldpc.encoding import LDPC5GEncoder
+from sionna.fec.ldpc.decoding import LDPC5GDecoder
+
+from sionna.mapping import Mapper, Demapper
+
+from sionna.utils import BinarySource, ebnodb2no, sim_ber
+from sionna.utils.metrics import compute_ber
+
+num_ut = 1
+num_bs = 1
+num_ut_ant = 2
+num_bs_ant = 16
+num_streams_per_tx = num_ut_ant
+rx_tx_association = np.array([[1]])
+sm = StreamManagement(rx_tx_association, num_streams_per_tx)
+rg = ResourceGrid(num_ofdm_symbols=14,
+                  fft_size=76,
+                  subcarrier_spacing=15e3,
+                  num_tx=1,
+                  num_streams_per_tx=num_streams_per_tx,
+                  cyclic_prefix_length=6,
+                  num_guard_carriers=[5,6],
+                  dc_null=True,
+                  pilot_pattern="kronecker",
+                  pilot_ofdm_symbol_indices=[2,11])
+carrier_frequency = 2.6e9 # Carrier frequency in Hz.
+                          # This is needed here to define the antenna element spacing.
+
+ut_array = AntennaArray(num_rows=1,
+                        num_cols=int(num_ut_ant/2),
+                        polarization="dual",
+                        polarization_type="cross",
+                        antenna_pattern="38.901",
+                        carrier_frequency=carrier_frequency)
+bs_array = AntennaArray(num_rows=1,
+                        num_cols=int(num_bs_ant/2),
+                        polarization="dual",
+                        polarization_type="cross",
+                        antenna_pattern="38.901",
+                        carrier_frequency=carrier_frequency)
+delay_spread = 300e-9 # Nominal delay spread in [s]. Please see the CDL documentation
+                      # about how to choose this value.
+
+direction = "uplink"  # The `direction` determines if the UT or BS is transmitting.
+                      # In the `uplink`, the UT is transmitting.
+cdl_model = "B"       # Suitable values are ["A", "B", "C", "D", "E"]
+
+speed = 10            # UT speed [m/s]. BSs are always assumed to be fixed.
+                      # The direction of travel will chosen randomly within the x-y plane.
+
+# Configure a channel impulse reponse (CIR) generator for the CDL model.
+# cdl() will generate CIRs that can be converted to discrete time or discrete frequency.
+cdl = CDL(cdl_model, delay_spread, carrier_frequency, ut_array, bs_array, direction, min_speed=speed)
+
+tdl = TDL(model="A",
+                  delay_spread=delay_spread,
+                  carrier_frequency=carrier_frequency,
+                  num_tx_ant=num_ut_ant,
+                  num_rx_ant=num_bs_ant)
+
+l_min, l_max = time_lag_discrete_time_channel(rg.bandwidth)
+l_tot = l_max-l_min+1
+a, tau = cdl(batch_size=2, num_time_steps=rg.num_time_samples+l_tot-1, sampling_frequency=rg.bandwidth)
+h_time = cir_to_time_channel(rg.bandwidth, a, tau, l_min=l_min, l_max=l_max, normalize=True)
+channel_time = ApplyTimeChannel(rg.num_time_samples, l_tot=l_tot, add_awgn=True)
+num_bits_per_symbol = 2 # QPSK modulation
+coderate = 0.5 # Code rate
+n = int(rg.num_data_symbols*num_bits_per_symbol) # Number of coded bits
+k = int(n*coderate) # Number of information bits
+
+# The binary source will create batches of information bits
+binary_source = BinarySource()
+
+# The encoder maps information bits to coded bits
+encoder = LDPC5GEncoder(k, n)
+
+# The mapper maps blocks of information bits to constellation symbols
+mapper = Mapper("qam", num_bits_per_symbol)
+
+# The resource grid mapper maps symbols onto an OFDM resource grid
+rg_mapper = ResourceGridMapper(rg)
+
+# The zero forcing precoder precodes the transmit stream towards the intended antennas
+zf_precoder = ZFPrecoder(rg, sm, return_effective_channel=True)
+
+# OFDM modulator and demodulator
+modulator = OFDMModulator(rg.cyclic_prefix_length)
+demodulator = OFDMDemodulator(rg.fft_size, l_min, rg.cyclic_prefix_length)
+
+
+num_bits_per_symbol = 2 # QPSK modulation
+coderate = 0.5 # Code rate
+n = int(rg.num_data_symbols*num_bits_per_symbol) # Number of coded bits
+k = int(n*coderate) # Number of information bits
+binary_source = BinarySource()
+
+
+batch_size = 8 # We pick a small batch_size as executing this code in Eager mode could consume a lot of memory
+ebno_db = 30
+perfect_csi = True
+
+no = ebnodb2no(ebno_db, num_bits_per_symbol, coderate, rg)
+b = binary_source([batch_size, 1, rg.num_streams_per_tx, encoder.k])
+c = encoder(b)
+x = mapper(c)
+x_rg = rg_mapper(x)
+
+# The CIR needs to be sampled every 1/bandwith [s].
+# In contrast to frequency-domain modeling, this implies
+# that the channel can change over the duration of a single
+# OFDM symbol. We now also need to simulate more
+# time steps.
+cir = cdl(batch_size, rg.num_time_samples+l_tot-1, rg.bandwidth)
+
+# OFDM modulation with cyclic prefix insertion
+x_time = modulator(x_rg)
+
+# Compute the discrete-time channel impulse reponse
+h_time = cir_to_time_channel(rg.bandwidth, *cir, l_min, l_max, normalize=True)
+
+# Compute the channel output
+# This computes the full convolution between the time-varying
+# discrete-time channel impulse reponse and the discrete-time
+# transmit signal. With this technique, the effects of an
+# insufficiently long cyclic prefix will become visible. This
+# is in contrast to frequency-domain modeling which imposes
+# no inter-symbol interfernce.
+y_time = channel_time([x_time, h_time, no])
+print(y_time.shape)
+pass
+

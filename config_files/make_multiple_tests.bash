00dd89fadff068d543cf31b90028eb3cdb584202
Ory Eger <egerory@gmail.com>
2026-05-03 15:51:38 +0300

    Mult and make_multiple_tests that were used for 4 anttenas completion

--- config_files/make_multiple_tests.bash
+++ config_files/make_multiple_tests.bash
@@ -10,10 +10,11 @@ input_file=$1
 base_name=$(basename "$input_file" .yaml)
 
 # ---------------- Parameters ----------------
-seeds=(123 17 41 58)
+seeds=(912 3008 1011 1806)
+#seeds=(17 58 41 123)
 # seeds=(123)
 # seeds=(17)
-snrs=($(seq 3 40))
+snrs=($(seq 10 40))
 cfos=(0)
 
 clip_percentage_in_tx_vals=(100)
@@ -25,27 +26,27 @@ shuffle_augment_priors_vals=(False)
 block_length_factor_vals=(3)
 
 # epochs sweep
-epochs_vals=(500)
+epochs_vals=(150)
 
 escnn_dropout_vals=(0.0)
 escnn_weight_decay_vals=(0.0)
-learning_rate_vals=(5.0e-4)
+learning_rate_vals=(5.0e-3)
 
-increase_prime_modulation_vals=(False)
-spatial_correlation_vals=('low')
+increase_prime_modulation_vals=(True)
+spatial_correlation_vals=('medium')
 
-batch_size_vals=(-1)
+batch_size_vals=(1024)
 
 which_augment_vals=(
   'AUGMENT_LMMSE'
 )
 
-channel_model_vals=('C' 'UMa' 'UMi' 'RMa')
+channel_model_vals=('C')
 kernel_size_vals=(3)
 run_tdfdcnn_vals=(False)
 
 pilot_size_vals=(20000)
-mcs_vals=(9)
+mcs_vals=(14 15)
 override_noise_var_vals=(False)
 
 mod_pilot_vals=(-1)

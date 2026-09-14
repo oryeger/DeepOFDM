9b6bf6d61026d931a100b55097980e9da9aaee6a
Ory Eger <egerory@gmail.com>
2026-09-14 08:56:57 +0300

    .

--- analyze_diag.bash
+++ analyze_diag.bash
@@ -17,8 +17,11 @@
 
 ### Hardcoded to the SNR=29 diag H5 the debug logging just produced - see analyze_diag_h5.py's
 ### docstring for why this doesn't need a config file (no training/EKF, just an LDPC decode
-### replay against the H5's own arrays).
-H5_FILE="/home/egero/Projects/Scratchpad/20260913_0853_TC_sp=0_QPSK_REs=96_UEs=1_ant=1_cfo=0.27_cfod=0.75_iqg=0_iqp=0_Clp=100_C=Lo_R=0.30_r=d2572a_frz=a_tf=0_dyn=ar1_a=0.99_sp0=0.1_sq=0.01_sr=0.5_spg=1_trk=sgd_csg=1_lr=0.005_ep=100_loss=bce_ps=20k_onvcfo_s=123_SNR=29_diag.h5"
+### replay against the H5's own arrays). Directory and filename kept separate so updating one
+### for a new run (usually just H5_NAME) doesn't require retyping the other.
+H5_DIR="/home/egero/Projects/Scratchpad"
+H5_NAME="20260913_1316_TC_sp=0_QPSK_REs=96_UEs=1_ant=1_cfo=0.27_cfod=0.75_iqg=0_iqp=0_Clp=100_C=Lo_R=0.30_r=d2572a_frz=a_tf=0_dyn=ar1_a=0.99_sp0=0.1_sq=0.01_sr=0.5_spg=1_trk=sgd_csg=1_lr=0.005_ep=100_loss=bce_ps=20k_onvbcfo_s=123_SNR=29_diag.h5"
+H5_FILE="$H5_DIR/$H5_NAME"
 
 echo `date`
 echo -e "\nSLURM_JOBID:\t\t" $SLURM_JOBID
